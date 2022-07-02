e1542ffe8e71cb9b997932c41f1b715a05514776
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-07-02 11:29:22 +0330

    Add ShoppingListEdit component into the ShoppingList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     RecipesComponent,
     ShoppingListComponent,
     RecipeDetailComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
