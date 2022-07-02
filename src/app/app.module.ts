b04648eb8e128700c2753bed6ca027931975aa15
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-07-02 10:58:51 +0330

    add ShoppingList component to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
