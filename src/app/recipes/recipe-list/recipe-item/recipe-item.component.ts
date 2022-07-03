298f008f2954841feb4c2ea3fb642dea113deae5
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-07-03 11:26:03 +0330

    Add RecipeService and use it to show RecipeList and RecipeDetail

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,14 +10,14 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe :Recipe;
-  @Output() selectedRecipe = new EventEmitter<Recipe>();
-  constructor() { }
+ 
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
   }
 
   onSelectRecipe(){
-    this.selectedRecipe.emit(this.recipe);
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './../recipe.model';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +8,13 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[]=[
-    new Recipe('Tasty Schnitzel', 'Tasty Schnitzel Description', 'https://www.marions-kochbuch.de/dru-pic/3936.jpg'),
-    new Recipe('Big Fat Burger', 'Big Fat Burger description', 'https://mypureplants.com/wp-content/uploads/2021/06/Vegan-Burger-Big-Mac-7.jpg')
-  ];
+  recipes: Recipe[];
 
-  @Output() selectedRecipe = new EventEmitter<Recipe>();
-  constructor() { }
+  
+  constructor(private recipeService:RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onSelectRecipe( recipe:Recipe){
-    this.selectedRecipe.emit(recipe)
-  }
 }
