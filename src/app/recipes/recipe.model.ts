de9e966ed18ce19daba825048e1d9b7435a25859
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-07-03 11:48:14 +0330

    Add ingredients property to RecipeModel

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,16 @@
+import { Ingredient } from 'src/app/shared/ingredient.modet';
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe{
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name:string, desc:string, imagepaht:string){
+    constructor(name:string, desc:string, imagepaht:string, ingredients:Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagepaht;
+        this.ingredients = ingredients;
     }
 }
