742cbda0cd350913901ae43dca7143b3e8ec8e70
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-07-07 11:01:33 +0330

    Clean the code in Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,10 @@
-import { EventEmitter } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 
 export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
+ 
     private recipes: Recipe[]=[
         new Recipe('Tasty Schnitzel', 
                     'Tasty Schnitzel Description', 
